154601bc57840aa6119e530aeaddd57cf7144aa6
Alina <alina.suchok00@gmail.com>
2026-03-19 11:09:08 +0200

    Replace System.out with SLF4J logger in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,18 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        System.out.println("5 + 3 = " + calculator.add(5, 3));
-        System.out.println("5 - 3 = " + calculator.subtract(5, 3));
-        System.out.println("5 * 3 = " + calculator.multiply(5, 3));
-        System.out.println("6 / 3 = " + calculator.divide(6, 3));
+        logger.info("5 + 3 = {}", calculator.add(5, 3));
+        logger.info("5 - 3 = {}", calculator.subtract(5, 3));
+        logger.info("5 * 3 = {}", calculator.multiply(5, 3));
+        logger.info("6 / 3 = {}", calculator.divide(6, 3));
     }
 }
